72a55a627135e069c829358ded38ba67472c7370
Nicholas Smith <nico.ehs05@gmail.com>
2018-09-20 17:31:07 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,15 +1,16 @@
 class Artist
   attr_accessor :name, :songs
- 
+    @@all_songs=[]
   def initialize(name)
     @name=name
     @songs=[]
   end
   
+  
+  
   def add_song(song)
     @songs << song
     song.artist=self
-    puts @songs
   end
   
   def add_song_by_name(name)
@@ -18,7 +19,7 @@ class Artist
   end
   
   def self.song_count
-    @songs.size
+    @@all_songs,count
   end
   
 end
