5269a6c8f6fbf048dc077160c57ee19cbd1ad941
Nicholas Smith <nico.ehs05@gmail.com>
2018-09-20 17:04:36 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,9 +1,21 @@
-class Author
+class Artist
   attr_accessor :name
  
-  def initialize
+  def initialize(name)
+    @name=name
+    @songs=[]
+  end
+  
+  def add_song
     
   end
- 
+  
+  def add_song_by_name
+    
+  end
+  
+  def song.song_count
+    
+  end
   
 end
